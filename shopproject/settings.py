0d19c94882f6e60b3279436846888412fdf1b1ed
chidera <chidera@gmail.com>
2024-11-04 16:38:37 -0800

    fix error

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(django_secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['tencis-stores.onrender.com', 'onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['tencis-stores.onrender.com']
 
 
 # Application definition
@@ -129,9 +129,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
