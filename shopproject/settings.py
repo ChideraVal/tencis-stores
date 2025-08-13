a4aa1c09d20e39342dee694bec282a91dda6bbf8
Chidera <pyjamel224@gmail.com>
2025-08-12 17:33:53 -0700

    Fix bug in trackorder template

--- shopproject/settings.py
+++ shopproject/settings.py
@@ -158,7 +158,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 #  CSRF cookie settings
